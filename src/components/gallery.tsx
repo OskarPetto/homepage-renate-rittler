7407f3a97c58fa22025d0dc0b0c8b1d6647fcd24
Oskar Petto <oskar.petto@hotmail.com>
2021-03-26 22:30:42 +0100

    remove react-bootstrap

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,6 +1,5 @@
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React, { FunctionComponent, useState } from 'react';
-import { Card, CardColumns, Modal } from 'react-bootstrap';
 import Painting from '../models/painting';
 import { Section } from '../models/section';
 import ContentService from '../services/content-service';
@@ -22,46 +21,33 @@ const Gallery: FunctionComponent<GalleryProps> = ({
       <h2 className="gallery-title" id={section.id}>
         {section.name}
       </h2>
-      <CardColumns>
+      <div className="gallery-paintings">
         {paintings.map((painting) => (
-          <Card
+          <div
+            role="button"
             className="painting"
             onClick={() => setOpenedPainting(painting)}
+            onKeyDown={() => setOpenedPainting(painting)}
+            tabIndex={0}
           >
-            <Card.Img
-              as={GatsbyImage}
+            <GatsbyImage
               image={ContentService.getSmallImage(painting)}
               alt={painting.title}
             />
 
-            <Card.Body>
+            <div className="painting-info">
               <h3 className="painting-title card-title">{painting.title}</h3>
               <div className="painting-subtitle">
                 {`${painting.year}, 
                   ${painting.technique}, 
                   ${painting.size}`}
               </div>
-            </Card.Body>
-          </Card>
+            </div>
+          </div>
         ))}
-      </CardColumns>
+      </div>
       {openedPainting !== undefined
-      && (
-      <Modal
-        show={openedPainting !== undefined}
-        onHide={() => setOpenedPainting(undefined)}
-      >
-        <Modal.Header closeButton />
-        <Modal.Body>
-          <GatsbyImage
-            image={
-            ContentService.getBigImage(openedPainting)
-          }
-            alt={openedPainting.title}
-          />
-        </Modal.Body>
-      </Modal>
-      )}
+      && <p>{openedPainting.id}</p>}
 
     </div>
   );
