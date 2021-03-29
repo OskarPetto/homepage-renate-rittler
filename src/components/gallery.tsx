ec88ee030ee69a435a0c6d677154709beda59190
Oskar Petto <oskar.petto@hotmail.com>
2021-03-30 01:11:36 +0200

    load big images

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,9 +1,28 @@
+import { useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React, { FunctionComponent, useState } from 'react';
+import Masonry from 'react-masonry-css';
 import Painting from '../models/painting';
 import { Section } from '../models/section';
 import ContentService from '../services/content-service';
 import './gallery.css';
+
+const paintingQuery = graphql`
+query PaintingQuery {
+  allFile {
+    edges {
+      node {
+        name
+        small: childImageSharp {
+        	gatsbyImageData(width: 400)
+        }
+        big: childImageSharp {
+        	gatsbyImageData(width: 2000)
+        }
+      }
+    }
+  }
+}`;
 
 interface GalleryProps {
   section: Section
@@ -13,6 +32,7 @@ const Gallery: FunctionComponent<GalleryProps> = ({
   section,
 }: GalleryProps) => {
   const [openedPainting, setOpenedPainting] = useState<Painting>(undefined);
+  const data = useStaticQuery(paintingQuery);
 
   const paintings = ContentService.getPaintings(section);
 
@@ -21,7 +41,16 @@ const Gallery: FunctionComponent<GalleryProps> = ({
       <h2 className="gallery-title" id={section.id}>
         {section.name}
       </h2>
-      <div className="gallery-paintings">
+      <Masonry
+        className="gallery-paintings"
+        // columnClassName="gallery-column"
+        breakpointCols={{
+          default: 4,
+          2000: 3,
+          1400: 2,
+          1000: 1,
+        }}
+      >
         {paintings.map((painting) => (
           <div
             role="button"
@@ -31,7 +60,7 @@ const Gallery: FunctionComponent<GalleryProps> = ({
             tabIndex={0}
           >
             <GatsbyImage
-              image={ContentService.getSmallImage(painting)}
+              image={ContentService.getSmallImage(data, painting)}
               alt={painting.title}
             />
 
@@ -45,10 +74,14 @@ const Gallery: FunctionComponent<GalleryProps> = ({
             </div>
           </div>
         ))}
-      </div>
+      </Masonry>
       {openedPainting !== undefined
-      && <p>{openedPainting.id}</p>}
-
+      && (
+      <GatsbyImage
+        image={ContentService.getBigImage(data, openedPainting)}
+        alt={openedPainting.title}
+      />
+      )}
     </div>
   );
 };
--- src/services/content-service.ts
+++ src/services/content-service.ts
@@ -1,4 +1,3 @@
-import { graphql, useStaticQuery } from 'gatsby';
 import { getImage } from 'gatsby-plugin-image';
 import ink from '../content/ink.yml';
 import oil from '../content/oil.yml';
@@ -7,20 +6,6 @@ import artist from '../content/artist.yml';
 import Painting from '../models/painting';
 import Section from '../models/section';
 import Artist from '../models/artist';
-
-const smallImageQuery = graphql`
-query SmallImageQuery {
-  allFile {
-    edges {
-      node {
-        name
-        childImageSharp {
-        	gatsbyImageData(width: 400)
-        }
-      }
-    }
-  }
-}`;
 
 const ContentService = {
 
@@ -39,15 +24,20 @@ const ContentService = {
     return paintings.filter((painting) => !painting.hide).sort((p1, p2) => p1.year - p2.year);
   },
 
-  getSmallImage(painting: Painting) {
-    const data = useStaticQuery(smallImageQuery);
+  getImageNode(data, painting: Painting) {
     const { edges } = data.allFile;
     const imageNode = edges.find(({ node }) => node.name === painting.id).node;
-    return getImage(imageNode);
+    return imageNode;
   },
 
-  getBigImage(painting: Painting) {
-    return this.getSmallImage(painting);
+  getSmallImage(data, painting: Painting) {
+    const imageNode = this.getImageNode(data, painting);
+    return getImage(imageNode.small);
+  },
+
+  getBigImage(data, painting: Painting) {
+    const imageNode = this.getImageNode(data, painting);
+    return getImage(imageNode.big);
   },
 
   getSections(): Section[] {
