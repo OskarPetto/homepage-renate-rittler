b0c02f2b9ff641b79f3795815690b308cd0f6133
Oskar Petto <oskar.petto@hotmail.com>
2021-04-02 18:03:09 +0200

    add SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import React, { FunctionComponent } from 'react';
+import { Helmet } from 'react-helmet';
 import Layout from '../components/layout';
 import Sidebar from '../components/sidebar';
 import Main from '../components/main';
+import artist from '../content/artist.yml';
 
 const Home: FunctionComponent = () => (
-  <Layout>
-    <Sidebar />
-    <Main />
-  </Layout>
+  <div>
+    <Helmet>
+      <title>{artist.name}</title>
+      <meta charSet="utf-8" /> */}
+      <meta name="description" content={`Künstlerische Arbeiten von ${artist.name}`} />
+      <meta name="language" content="de-AT" />
+      <meta name="robots" content="index, follow" />
+      <link rel="canonical" href="https://renaterittler.art"/>
+    </Helmet>
+
+    <Layout>
+      <Sidebar />
+      <Main />
+    </Layout>
+  </div>
 );
 
 export default Home;
